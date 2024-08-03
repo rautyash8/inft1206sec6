e3353e747853f747f273417e88f4d5f4f7f646ef
rautyash8 <rautyash675@gmail.com>
2024-08-02 23:04:49 -0400

    Shape and Ball Class Definitions

--- Assignment-4/JS/A4P4.js
+++ Assignment-4/JS/A4P4.js
@@ -18,3 +18,64 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Shape {
+    constructor(x, y, velX, velY){
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
+
+class Ball extends Shape {
+
+   constructor(x, y, velX, velY, color, size, exists) {
+      super(x,y,velX,velY);
+      this.color = color;
+      this.size = size;
+      this.exists = true;
+   }
+
+   draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+   }
+
+   update() {
+      if ((this.x + this.size) >= width) {
+         this.velX = -(Math.abs(this.velX));
+      }
+
+      if ((this.x - this.size) <= 0) {
+         this.velX = Math.abs(this.velX);
+      }
+
+      if ((this.y + this.size) >= height) {
+         this.velY = -(Math.abs(this.velY));
+      }
+
+      if ((this.y - this.size) <= 0) {
+         this.velY = Math.abs(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+   }
+
+   collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }  
+}
