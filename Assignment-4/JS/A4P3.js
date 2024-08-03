a3fe6f295a6eb661162c5a0cda26d078c1a5605c
rautyash8 <rautyash675@gmail.com>
2024-08-02 22:54:41 -0400

    Animation Loop Function

--- Assignment-4/JS/A4P3.js
+++ Assignment-4/JS/A4P3.js
@@ -114,3 +114,16 @@ while (balls.length < 25) {
 
     balls.push(ball);
 }
+
+function loop() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
