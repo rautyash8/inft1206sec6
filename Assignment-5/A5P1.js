37ca423c86f477275d7e65943e9401b544d3d341
rautyash8 <rautyash675@gmail.com>
2024-08-09 22:11:50 -0400

    Show/Hide Button Initialization

---
+++ Assignment-5/A5P1.js
@@ -0,0 +1,7 @@
+// functionality for showing/hiding the comments section
+
+var showHideBtn = document.querySelector('.show-hide');
+var commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
