6e29b45b002184ecf00b56a5c6caf01f7aa755a8
rautyash8 <rautyash675@gmail.com>
2024-08-09 22:12:36 -0400

    Show/Hide Button Click Event

--- Assignment-5/A5P1.js
+++ Assignment-5/A5P1.js
@@ -5,3 +5,14 @@ var commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
+showHideBtn.onclick = function() {
+    var showHideText = showHideBtn.textContent;
+    if(showHideText === 'Show comments') {
+      showHideBtn.textContent = 'Hide comments';
+      commentWrapper.style.display = 'block';
+    } else {
+      showHideBtn.textContent = 'Show comments';
+      commentWrapper.style.display = 'none';
+    }
+};
+
